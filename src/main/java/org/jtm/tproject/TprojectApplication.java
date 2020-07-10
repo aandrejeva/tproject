e5111eb798a249dba39d870c1eda6c36bac27f28
aandrejeva <ak08548@inbox.lv>
2020-07-10 15:18:50 +0300

    Initial commit

---
+++ src/main/java/org/jtm/tproject/TprojectApplication.java
@@ -0,0 +1,13 @@
+package org.jtm.tproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jtm/tproject/TprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jtm.tproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
